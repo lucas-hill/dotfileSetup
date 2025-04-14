5039211300104b62a62fda7517cc1a03b72a7f6d
Lucas Hill <lucas10hill@gmail.com>
2025-04-13 20:04:13 -0600

    Updated the install command to use the installer setup and filter out the appropriate packages for the operating system.

--- cmd/install.go
+++ cmd/install.go
@@ -5,25 +5,47 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/lucas-hill/dotfileSetup/internal/installers"
 	"github.com/lucas-hill/dotfileSetup/internal/setup"
+	"github.com/lucas-hill/dotfileSetup/internal/tui/selectlist"
 	"github.com/spf13/cobra"
 )
 
 var installCmd = &cobra.Command{
 	Use:   "install [components]",
 	Short: "Install one or more dotfiles components",
-	Args:  cobra.MinimumNArgs(1),
+	Args:  cobra.MinimumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
-		for _, component := range args {
-			fmt.Printf("Installing %s...\n", component)
-			repoDir := filepath.Join(os.Getenv("HOME"), "dotfiles")
 
-			switch component {
-			case "zsh":
-				setup.SetupZsh(repoDir)
-			case "docker":
-				setup.SetupDocker()
+		repoDir := filepath.Join(os.Getenv("HOME"), "dotfiles")
+
+		//NOTE:
+		//placing the installer here so we can filter out the packages we can access
+		installer, err := installers.GetInstaller()
+		if err != nil {
+			os.Exit(1)
+		}
+
+		var selected []string
+		if len(args) > 0 {
+			selected = installer.FilterSupportedPackages(args)
+		} else {
+			packagesInRepo, err := setup.ListManagedPackages(repoDir)
+			if err != nil {
+				fmt.Printf("Packages to install could not be found %v\n", err)
 			}
+			availablePackages := installer.FilterSupportedPackages(packagesInRepo)
+			selected, err = selectlist.MultiSelect("Select packages to setup", availablePackages)
+			if err != nil {
+				fmt.Printf("Selection failed: %v\n", err)
+				os.Exit(1)
+			}
+		}
+
+		err = installer.Install(selected, repoDir)
+		if err != nil {
+			fmt.Printf("Installation failed %v\n", err)
+			os.Exit(1)
 		}
 	},
 }
